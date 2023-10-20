76887b9da6893c9a0aced53862f6c9d4e25cd0f6
Eddie Sandler <ed.sandler@gmail.com>
2023-10-20 16:27:33 -0400

    added board to session and created form

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ from flask import Flask,render_template,flash,session
 
 
 
+
+
 app=Flask(__name__)
+app.secret_key='bogglemebonkers'
 
 @app.route('/')
+
 def start_bogle():
+    '''instantiates an instance of Boggle and creates a game board'''
     game = Boggle()
     board=game.make_board()
-
-    return render_template('board.html',game=game,board=board)
+    session['key']=board
+    msg=flash('Play Boggle')
+    return render_template('board.html',game=game,board=board,messages=msg)
 
 
 
