421a0abd1bd1f3fa0ce1ec124edd0f8972a6c6a5
Eddie Sandler <ed.sandler@gmail.com>
2023-10-23 16:54:14 -0400

    score and timer working. need to add to DOM

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from boggle import Boggle
-from flask import Flask,render_template,flash,session,request,redirect
+from flask import Flask, render_template, flash, session, request, redirect,jsonify
 
 
-
+app = Flask(__name__)
+app.secret_key = 'bogglemebonkers'
+app.static_folder = 'static'
 game = Boggle()
 
-app=Flask(__name__)
-app.secret_key='bogglemebonkers'
-app.static_folder = 'static'
 
 @app.route('/')
 def start_game():
@@ -17,19 +16,26 @@ def start_game():
 
 @app.route('/make_board')
 def start_bogle():
-    '''instantiates an instance of Boggle and creates a game board'''
+    '''creates a game board'''
 
-    board=game.make_board()
-    session['key']=board
-    msg=flash('Play Boggle')
-    return render_template('board.html',game=game,board=board,messages=msg)
+    board = game.make_board()
+    session['key'] = board
+    msg = flash('Play Boggle')
+    return render_template('board.html', game=game, board=board, messages=msg)
 
-@app.route('/get_input/',methods=['POST'])
+
+@app.route('/get_input/', methods=['POST'])
 def get_input():
     guess = request.json.get("word")
     session['guess'] = guess
     print(f'{guess} sent to the backend')
+    board = session['key']
+    is_valid = game.check_valid_word(board, guess)
 
+    if is_valid == 'ok':
+        print(f'word is valid status: {is_valid}')
+    else:
+        print( f'word is invalid status: {is_valid}')
+    return jsonify(is_valid=is_valid)
 
-    return guess
 
--- static/app.js
+++ static/app.js
@@ -1,16 +1,53 @@
+const used_words = [];
+let score = 0;
+let timeLeft = 60; // Initial time in seconds
+
+// Function to update the timer
+function updateTimer() {
+  if (timeLeft > 0) {
+    // console.log(`Time left: ${timeLeft} seconds`);
+    timeLeft--;
+  } else {
+    console.log('Time is up!');
+    // You can add game-over logic here
+    clearInterval(timerInterval); // Stop the timer
+
+    const wordInput = document.getElementById("guess");
+    wordInput.disabled = true; // Disable the input field
+  }
+}
+
+// Start the timer
+const timerInterval = setInterval(updateTimer, 1000); // Update every second
 
 
 async function playBoggle(e) {
   e.preventDefault();
 
+
   const wordInput = document.getElementById("guess");
   const word = wordInput.value;
   console.log('Word is:', word);
 
-   const response=await axios.post("/get_input/", { word: word })
-   console.log(response.config.data)
 
-   return response
+  const response = await axios.post("/get_input/", { word: word });
+
+  const is_valid = response.data.is_valid;
+  console.log('status:', is_valid);
+
+  if(used_words.includes(word)){
+    console.log('try again asshole- you already used that')
+  }
+  if(is_valid==='ok' && !used_words.includes(word)){
+    console.log('good word')
+    used_words.push(word)
+    score += word.length
+    console.log(used_words)
+    console.log('score',score)
+  }
+
+  wordInput.value = '';
+  return response;
 }
 
 const form = document.getElementById('input-Word');
