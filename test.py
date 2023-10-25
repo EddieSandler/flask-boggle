60763a77febfe74c344781d1ccdb74d8d932620a
EddieSandler <ed.sandler@gmail.com>
2023-10-25 17:31:47 -0400

    testing in progress

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+
+    def test_start_game(self):
+        with app.test_client() as client:
+            import pdb
+            pdb.set_trace()
+
+    
+
+   
